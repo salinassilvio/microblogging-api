c5f762ced4a6026a52d67f1a99bb5d9f1f9b1189
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-04-25 20:28:04 -0600

    configurando server listen

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const server = require('http').Server(app);
 const debug = require('debug')('app:server');
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-debug(`Servidor corriendo en el puerto : ${server.address().port}`);
-});
+// server.listen(port, () => {
+// debug(`Servidor corriendo en el puerto : ${server.address().port}`);
+// });
+
+server.listen(port,'0.0.0.0');
